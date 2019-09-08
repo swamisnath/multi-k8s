088f6abb99ec378e34c27f4bdec51b93d18a15fc
swaminath.subramanyam@gmail.com <Swaminath.Subramanyam@gmail.com>
2019-09-08 18:31:19 -0500

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t sswaminath/multi-client:latest -t sswaminath/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t sswaminath/multi-server:latest -t sswaminath/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t sswaminath/multi-worker:latest -t sswaminath/multi-worker:$SHA -f ./WORKER/Dockerfile ./worker
+
+docker push sswaminath/multi-client:latest
+docker push sswaminath/multi-server:latest
+docker push sswaminath/multi-worker:latest
+
+docker push sswaminath/multi-client:$SHA
+docker push sswaminath/multi-server:$SHA
+docker push sswaminath/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=sswaminath/multi-server:$SHA
+kubectl set image deployments/client-deployment client=sswaminath/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=sswaminath/multi-worker:$SHA
